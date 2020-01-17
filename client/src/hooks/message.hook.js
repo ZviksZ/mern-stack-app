9f5fe1e7278ed5c6dca5c72787bd147d31083471
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-01-17 20:46:37 +0300

    register, login, logout

--- client/src/hooks/message.hook.js
+++ client/src/hooks/message.hook.js
@@ -2,7 +2,7 @@ import {useCallback} from 'react'
 
 export const useMessage = () => {
    return useCallback(text => {
-      if (window.matchMedia && text) {
+      if (window.M && text) {
          window.M.toast({html: text})
       } 
    }, [])
